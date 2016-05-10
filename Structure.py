4bd028d2ef80a98d509a15508f87e311f0c0b34f
nastyaProdan <CrazyFish193@ukr.net>
2016-05-11 00:47:09 +0300

    Update Structure.py

--- Structure.py
+++ Structure.py
@@ -11,6 +11,15 @@ class CalendarDay:
         self.systolic_pressure = systolic_pressure
         self.pulse = pulse
 
+    def getList(self):
+        dict = {"day" : self.day, "month": self.month ,"year" : self.year ,
+                "times_of_day": self.times_of_day ,
+                "diastolic_pressure" :self.diastolic_pressure ,
+                "systolic_pressure" : self.systolic_pressure ,"pulse" : self.pulse }
+        return dict
+    def self_print(self):
+        print (self.day,self.month ,self.year, self.times_of_day ,self.diastolic_pressure, self.systolic_pressure ,self.pulse)
+
     def setNewTimes_of_day(self, times_of_day):
         self.times_of_day = times_of_day
 
@@ -23,14 +32,15 @@ class CalendarDay:
     def setNewPulse(self, pulse):
         self.pulse = pulse
 
-    def __repr__(self):
-        return "{!r}.{!r}.{!r}.{!r}.{!r}.{!r}".format(self.day, self.month, self.year, self.times_of_day, self.diastolic_pressure, self.pulse)
-
-    def __str__(self):
-        return "{!r}.{!r}.{!r}".format(self.day, self.month, self.year)
-
-
 diary = []
+diaryToSave = []
+
+def run(temp):
+    for i in temp :
+        diaryToSave.append(i.getList())
+    return diaryToSave
+
+
 
 def getAllRecords():
     """
@@ -41,21 +51,6 @@ def getAllRecords():
     """
     return diary
 
-def loading_f():
-    global diary
-    try:
-        f = open('diary.pickle', "rb")
-        diary = pickle.load(f)
-    except:
-        pass
-    finally:
-        f.close()
-
-
-def save():
-    f = open('diary.pickle', 'wb')
-    pickle.dump(diary, f)
-    f.close()
 
 def CreateRecord(day, month, year, times_of_day, diastolic_pressure, systolic_pressure, pulse):
     """
@@ -76,8 +71,9 @@ def CreateRecord(day, month, year, times_of_day, diastolic_pressure, systolic_pr
             print("Error! Record already exists")
             break
     else:
+        #a = CalendarDay(1, 2, 3, "evening", 1, 2, 3)
         diary.append(CalendarDay(day, month, year, times_of_day, diastolic_pressure, systolic_pressure, pulse))
-        save()
+
 
 def is_date_valid(day, month):
     """
@@ -90,24 +86,28 @@ def is_date_valid(day, month):
     >>> is_date_valid(12,12)
     True
     """
-    a = int(day)
-    b = int(month)
-    if (a > 31):
-        return False
-    elif (b > 12):
-        return False
-    elif (a == 0):
-        return False
-    elif (b == 0):
+    a=int(day)
+    b= int(month)
+    if (a > 31) or (b > 12):
         return False
     else:
         return True
 
 def is_year_valid(year):
-     if (int(year) > 0):
-         return True
-     else:
-         return False
+    """
+    Функция проверяет корректность введенного дня и месяца, их попадание в допустимый диапазон
+    :param day: день, который необходимо сверить с допустимым диапазоном
+    :param month: месяц, который необходимо сверить с допустимым диапазоном
+    :return: корректно ли были введены день и месяц
+    >>> is_year_valid("hgbjn")
+    False
+    >>> is_year_valid(2000)
+    True
+    """
+    if year.isdigit():
+      return True
+    else:
+      return False
 
 
 
@@ -179,4 +179,26 @@ def is_pulse_norm(pulse):
         return True
     else:
         return False
+
+def read_serialization_type(key1):
+    # Выводит меню для выбора типа сериализации
+    while True:
+        if key1 == "1":
+            return 'pickle'
+        elif key1 == "2":
+            return 'yaml'
+        elif key1 == "3":
+            return 'json'
+        else:
+            print("Wrong input! Repeat please...")
+            break
+
+
+
+
+def change_serialization_file(key1):
+    # Считывает имя файла с которым пользователь хочет работать.
+    #  Передает имя файла и его тип .
+    inp = input("Enter your file name(without file format) : ")
+    return read_serialization_type(key1), inp
 
